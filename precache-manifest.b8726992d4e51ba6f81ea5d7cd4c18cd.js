46b883538ae46880923ff5abc3d2ce3775b4c8f0
arkhurst <fiiarkhurst@gmail.com>
2020-06-08 06:28:55 -1200

    Updates

---
+++ precache-manifest.b8726992d4e51ba6f81ea5d7cd4c18cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77c1781893da01cadb8368c8753c179",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "38afc39fcdd2567a19bc",
+    "url": "/my-portfolio/static/css/main.b9ee088a.chunk.css"
+  },
+  {
+    "revision": "6f8d4390bc754925a620",
+    "url": "/my-portfolio/static/js/2.2fe9fc13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.2fe9fc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38afc39fcdd2567a19bc",
+    "url": "/my-portfolio/static/js/main.db47fdcb.chunk.js"
+  },
+  {
+    "revision": "170f45050b91094c3a2e",
+    "url": "/my-portfolio/static/js/runtime-main.354f34ae.js"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/my-portfolio/static/media/background.00051dbb.jpg"
+  },
+  {
+    "revision": "1a11f18d1054cc01dff96024627db508",
+    "url": "/my-portfolio/static/media/portrait.1a11f18d.jpg"
+  },
+  {
+    "revision": "8952d6a79cddded28a43f3f0735788ce",
+    "url": "/my-portfolio/static/media/portrait_small.8952d6a7.jpg"
+  },
+  {
+    "revision": "2efd960d6f072c1970115149dc9607d6",
+    "url": "/my-portfolio/static/media/project1.2efd960d.jpg"
+  },
+  {
+    "revision": "473d9f55d2f5045e41e3271c338cdb11",
+    "url": "/my-portfolio/static/media/project2.473d9f55.jpg"
+  },
+  {
+    "revision": "139308d797d4a548920ce783be94237f",
+    "url": "/my-portfolio/static/media/project3.139308d7.jpg"
+  },
+  {
+    "revision": "5a100b46aa5dcadfabc907a76cf78c87",
+    "url": "/my-portfolio/static/media/project4.5a100b46.jpg"
+  },
+  {
+    "revision": "babc9580b3b97699fb8a21b526912d87",
+    "url": "/my-portfolio/static/media/project5.babc9580.jpg"
+  }
+]);
